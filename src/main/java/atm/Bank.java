29f4a50592ba708ce5a3a1260edfbe3b5c2e535e
sidtipat.k <s.kietchai@outlook.com>
2020-09-19 22:52:42 +0700

    add xml

--- src/main/java/atm/Bank.java
+++ src/main/java/atm/Bank.java
@@ -1,43 +1,46 @@
 package atm;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import java.util.Map;
 
 /**
  * A bank contains customers with bank accounts.
  */
+@Component
 public class Bank {
 
-    private String name;
-    private Map<Integer, Customer> customers;
-    private DataSource dataSource;
+   private String name;
+   private Map<Integer,Customer> customers;
+   private DataSource dataSource;
 
-    /**
-     * Constructs a bank with no customers.
-     */
-    public Bank(String name, DataSource dataSource) {
-        this.name = name;
-        this.dataSource = dataSource;
-        this.customers = dataSource.readCustomers();
-    }
+   /**
+    * Constructs a bank with no customers.
+    */
+   @Autowired
+   public Bank(String name, DataSource dataSource) {
+      this.name = name;
+      this.dataSource = dataSource;
+      this.customers = dataSource.readCustomers();
+   }
 
-    /**
-     * Adds a customer to the bank.
-     *
-     * @param customer the customer to add
-     */
-    public void registerCustomer(Customer customer) {
-        customers.put(customer.getId(), customer);
-    }
+   /**
+    * Adds a customer to the bank.
+    * @param customer the customer to add
+    */
+   public void registerCustomer(Customer customer) {
+      customers.put(customer.getId(), customer);
+   }
 
-    /**
-     * Finds a customer in the bank.
-     *
-     * @param id a customer id
-     * @return the matching customer, or null if no customer
-     * matches
-     */
-    public Customer findCustomer(int id) {
-        return customers.get(id);
-    }
+   /**
+    * Finds a customer in the bank.
+    * @param id a customer id
+    * @return the matching customer, or null if no customer
+    * matches
+    */
+   public Customer findCustomer(int id) {
+      return customers.get(id);
+   }
 }
 
--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -1,17 +1,13 @@
 package atm;
 
-import atm.config.ATMConfig;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     public static void main(String[] args) {
-//      CodeConfig-based
         ApplicationContext context =
-                new AnnotationConfigApplicationContext(ATMConfig.class);
-
-        AtmUI atmUI = context.getBean(AtmUI.class);
+                new ClassPathXmlApplicationContext("bean.xml");
+        AtmUI atmUI = context.getBean("atmUI", AtmUI.class);
         atmUI.run();
     }
 }
